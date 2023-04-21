22572f8ecd2d55097ab63300fd0a376c3e845007
phamquang-hieu <phamhieu1906@gmail.com>
2023-04-21 13:36:30 +0700

    test oob

--- X-CLIP/datasets/pipeline.py
+++ X-CLIP/datasets/pipeline.py
@@ -2108,7 +2108,7 @@ class SampleAnnotatedFrames(SampleFrames):
             # this is the case if the video is of a normal action
             # randomly sample an interval of around 10 times larger than the number of frame extracted to feed into the model
             total_frames = results["total_frames"]
-            start = np.random.randint(0, total_frames)
+            start = np.random.randint(0, total_frames-1)
             end = start + 10 * self.num_clips * self.clip_len
             if end > total_frames:
                 end = total_frames - 1
