0712e3d6596273d8118446eda1a3e40e05af731f
phamquang-hieu <phamhieu1906@gmail.com>
2023-05-04 10:05:14 +0700

    test oom

--- X-CLIP/main.py
+++ X-CLIP/main.py
@@ -140,7 +140,7 @@ def train_one_epoch(epoch, model, criterion, optimizer, lr_scheduler, train_load
 
         images = batch_data["imgs"].cuda(non_blocking=True)
         label_id = batch_data["label"].cuda(non_blocking=True)
-        batch_data = None
+        # batch_data = None
         del batch_data
         gc.collect()
         label_id = label_id.reshape(-1)
