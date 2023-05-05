29b59478a37597c475c8357b9ad30a2c4ee21ade
phamquang-hieu <phamhieu1906@gmail.com>
2023-05-05 09:52:23 +0700

    typo

--- X-CLIP/datasets/pipeline.py
+++ X-CLIP/datasets/pipeline.py
@@ -927,7 +927,7 @@ class MultiScaleCrop(RandomCrop):
                                                      crop_bbox)
             if 'imgs' in results:
                 #@PQH: assume that imgs is already a np array
-                assert type(results['imgs']) == np.ndarray & len(results['imgs'].shape) == 4
+                assert type(results['imgs']) == np.ndarray and len(results['imgs'].shape) == 4
                 x1, y1, x2, y2 = crop_bbox
                 results['imgs'] = results['imgs'][:, y1:y2, x1:x2, :]
         else:
