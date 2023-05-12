86396b2f428deae746c5a9b3828bb3eff7fe100c
phamquang-hieu <phamhieu1906@gmail.com>
2023-05-12 14:47:19 +0700

    text_inputs.shape[1] instead of 0

--- X-CLIP/main.py
+++ X-CLIP/main.py
@@ -280,7 +280,7 @@ def validate_2stage(val_loader, text_labels_1, text_labels_2, text_id:np.ndarray
     model.eval()
     def views_inference(text_inputs, label_id):
         print(text_inputs.shape)
-        tot_similarity = torch.zeros((b, text_inputs.shape[0])).cuda()
+        tot_similarity = torch.zeros((b, text_inputs.shape[1])).cuda()
         for i in range(n): # for view in views
             image = _image[:, i, :, :, :, :] # [b,t,c,h,w]
             label_id = label_id.cuda(non_blocking=True)
