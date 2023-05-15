97726d0354a64779781f565ecedab09df783c163
phamquang-hieu <phamhieu1906@gmail.com>
2023-05-15 17:13:32 +0700

    test

--- X-CLIP/main.py
+++ X-CLIP/main.py
@@ -319,7 +319,7 @@ def validate_2stage(val_loader, text_labels_1, text_labels_2, text_id_1:np.ndarr
         y_true, y_pred = [], []
         # top1y_log, top5y_log = [], []
         for idx, batch_data in enumerate(val_loader):
-            if idx < 104: continue
+            # if idx < 104: continue
             _image = batch_data["imgs"]
             label_id = batch_data["label"]
             label_id = label_id.reshape(-1)
@@ -342,6 +342,7 @@ def validate_2stage(val_loader, text_labels_1, text_labels_2, text_id_1:np.ndarr
 
             acc5_meter.update(float(acc5) / b * 100, b)
             indices_5 = indices_5.cpu()
+            indices_5 = [text_id_1[index] for index in indices_5]
             # print("hey", text_inputs_2.shape, text_inputs_2[indices_5[i], :].shape, text_inputs_1.shape)
             for i in range(b):
                 # print("hej", text_id_2, text_id_2.reshape(1, -1))
