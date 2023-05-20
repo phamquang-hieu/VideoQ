5f9f467d530da80c084ddaedff955924c6db3cd1
phamquang-hieu <phamhieu1906@gmail.com>
2023-05-20 09:36:52 +0700

    no freeze class_embedding

--- X-CLIP/models/xclip.py
+++ X-CLIP/models/xclip.py
@@ -161,7 +161,7 @@ class XCLIP(CLIP):
     
     def freeze_video_encoder(self): 
         for name, module in self.named_modules():
-            if 'visual.prompt_pool' in name or "mit." in name:
+            if 'visual.prompt_pool' in name or "mit." in name or "visual.class_embedding" in name:
                 module.requires_grad_(True)
             else:
                 module.requires_grad_(False)
