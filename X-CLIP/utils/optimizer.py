727cb4f02ff66246935658be996ec3fad193260c
phamquang-hieu <phamhieu1906@gmail.com>
2023-05-24 11:23:21 +0700

    larger scale for text prompt

--- X-CLIP/utils/optimizer.py
+++ X-CLIP/utils/optimizer.py
@@ -71,7 +71,7 @@ def build_optimizer(config, model):
         have=("prompts",), not_have=()
     )
     text_soft_prompts = set_weight_decay(model, skip, skip_keywords,
-        weight_decay=config.TRAIN.WEIGHT_DECAY, lr=config.TRAIN.LR*1000,
+        weight_decay=config.TRAIN.WEIGHT_DECAY, lr=config.TRAIN.LR*10000,
         have=("prefix", "postfix",), not_have=()
         )
     
