eff5c7dd6b8ac0fd8ffc5d7636f2b11cd5c6ee46
phamquang-hieu <phamhieu1906@gmail.com>
2023-05-27 10:39:23 +0700

    bugfix typo

--- X-CLIP/models/prompt.py
+++ X-CLIP/models/prompt.py
@@ -79,7 +79,7 @@ class VideoSpecificPrompt(nn.Module):
 
 
     def _init_weights(self, m):
-        if isinstance(m, nn.Linear):58
+        if isinstance(m, nn.Linear):
             trunc_normal_(m.weight, std=.02)
             if isinstance(m, nn.Linear) and m.bias is not None:
                 nn.init.constant_(m.bias, 0)
