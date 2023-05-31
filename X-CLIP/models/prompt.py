4a85e088b3830f3c38cde63039dc6ae7577003af
phamquang-hieu <phamhieu1906@gmail.com>
2023-05-31 20:28:11 +0700

    normalize input not values

--- X-CLIP/models/prompt.py
+++ X-CLIP/models/prompt.py
@@ -114,7 +114,8 @@ class PromptPool(nn.Module):
         self.prompt_freq = self.prompt_freq.to(self.keys.device)
         with torch.no_grad():
             self.keys.data = self.keys.data/self.keys.data.norm(dim=-1).unsqueeze(dim=-1)
-            self.values.data = self.values.data/self.values.data.norm(dim=-1).unsqueeze(dim=-1)
+            # self.values.data = self.values.data/self.values.data.norm(dim=-1).unsqueeze(dim=-1)
+            x.data = x.data/x.data.norm(dim=-1).unsqueeze(dim=-1)
 
         if self.use_freq:
             penalty = 1 - self.prompt_freq/self.prompt_freq.sum() #[1, pool_size]
