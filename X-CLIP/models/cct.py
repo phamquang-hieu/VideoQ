b04e132c1b79bb00e518f3da08b714401a8133e0
phamquang-hieu <phamhieu1906@gmail.com>
2023-06-03 21:51:29 +0700

    test

--- X-CLIP/models/cct.py
+++ X-CLIP/models/cct.py
@@ -146,7 +146,7 @@ class CrossFrameCommunicationTransformer(nn.Module):
                 query = self.transformer(query)
                 query = query.permute(1, 0, 2)
                 query = self.ln_post(query[:, 0, :])
-                query = query[:, 0, :].unsqueeze(1)
+                query = query.unsqueeze(1)
             
             prompt, prompt_key_loss = self.prompt_pool(query)
             # prompt_key_loss = None
