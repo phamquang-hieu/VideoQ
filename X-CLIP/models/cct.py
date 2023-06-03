6f77fd98d501a0a1eb89ed258c18a3486a927a60
phamquang-hieu <phamhieu1906@gmail.com>
2023-06-03 17:39:13 +0700

    test

--- X-CLIP/models/cct.py
+++ X-CLIP/models/cct.py
@@ -150,15 +150,11 @@ class CrossFrameCommunicationTransformer(nn.Module):
             
             # print("query.requires_grad", query.requires_grad)
             prompt, prompt_key_loss = self.prompt_pool(query)
-            print("prompt.requires_grad", prompt.requires_grad)
-            print("x.requires_grad", x.requires_grad)
             x = torch.cat([prompt, x], dim=1)
         
         x = self.ln_pre(x)
         x = x.permute(1, 0, 2) # [b*t, grid**2 + 1(class token), width]-> [grid**2 + 1 (class token), (b*t), width] this is needed for multihead self attn when batch_first = false
         x = self.transformer(x)
-        for name, param in self.transformer.named_parameters():
-            print(param.requires_grad)
         x = x.permute(1, 0, 2)
         prompt_idx = self.pool_prompt_per_sample*self.pool_prompt_length 
         # if prompt_idx == 0: 
