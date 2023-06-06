2fede3607e436000d9b7d34ba48e17e1c37f972f
phamquang-hieu <phamhieu1906@gmail.com>
2023-06-07 00:07:02 +0700

    test

--- X-CLIP/main.py
+++ X-CLIP/main.py
@@ -199,6 +199,7 @@ def train_one_epoch(epoch, model, criterion, optimizer, lr_scheduler, train_load
             
             if config.TRAIN.SYMMETRIC_LOSS:
                 one_hot = torch.zeros(output.shape[1], output.shape[0])
+                print(batch_data['label'])
                 one_hot.scatter_(1, batch_data['label'], 1.0).to(batch_data['label'].device)
                 total_loss += criterion(output.t(), label_id)
 
