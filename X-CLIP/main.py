7535122bd6c155b734f6935b319b206e4f04fffc
phamquang-hieu <phamhieu1906@gmail.com>
2023-06-09 09:12:15 +0700

    test no decay

--- X-CLIP/main.py
+++ X-CLIP/main.py
@@ -221,14 +221,14 @@ def train_one_epoch(epoch, model, criterion, optimizer, lr_scheduler, train_load
                 scaler.step(optimizer)
                 scaler.update()
                 optimizer.zero_grad()
-                # if epoch < config.TRAIN.WARMUP_EPOCHS:
-                lr_scheduler.step_update(epoch * num_steps + idx)
+                if epoch < config.TRAIN.WARMUP_EPOCHS:
+                    lr_scheduler.step_update(epoch * num_steps + idx)
         else:
             # optimizer.step()
             scaler.step(optimizer)
             scaler.update()
-            # if epoch < config.TRAIN.WARMUP_EPOCHS:
-            lr_scheduler.step_update(epoch * num_steps + idx)
+            if epoch < config.TRAIN.WARMUP_EPOCHS:
+                lr_scheduler.step_update(epoch * num_steps + idx)
 
         torch.cuda.synchronize()
         
