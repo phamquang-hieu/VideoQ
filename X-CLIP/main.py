b152545d4928de8a6137bbf0e468329fd1c41068
phamquang-hieu <phamhieu1906@gmail.com>
2023-06-16 12:09:10 +0700

    test KLDiv

--- X-CLIP/main.py
+++ X-CLIP/main.py
@@ -214,6 +214,12 @@ def train_one_epoch(epoch, model, criterion, optimizer, lr_scheduler, train_load
                 # print(criterion(output, label_id), criterion(output.t().contiguous(), one_hot))
                 total_loss = 0.5*(criterion(output, label_id) + criterion(output.t().contiguous(), one_hot))
             else:
+                if isinstance(criterion, nn.KLDivLoss):
+                # label_id = nn.functional.one_hot(label_id, num_classes=config.DATA.NUM_CLASSES).to(torch.float32)
+                    if config.AUG.LABEL_SMOOTH:
+                        # label_id = label_id*(1-config.AUG.LABEL_SMOOTH) + config.AUG.LABEL_SMOOTH/label_id.shape[-1]
+                        output = nn.functional.log_softmax(output, dim=-1)
+                        label_id = label_id.log()
                 total_loss = criterion(output, label_id)
             
             if prompt_key_loss is not None:
